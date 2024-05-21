18cb4978ce05d2d065373a6e8a893d201e778364
afrodriguezu <andrey498@hotmail.com>
2024-05-20 21:58:59 -0500

    Config Plantillas, rutas y peticiones

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 /* -------------------------- Importar dependecias -------------------------- */
 import express from 'express';
+import morgan from 'morgan';
+import {join, dirname} from 'path'
+import {fileURLToPath} from 'url'
+import {engine} from 'express-handlebars'
 
 /* ----------------------------- Initializacion ----------------------------- */
 const app = express();
+/* Evitar colisiones de nombre de variable*/
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /* --------------------------------- Setting -------------------------------- */
 app.set('port', process.env.PORT || 3000)
+app.set('views', join(__dirname, 'views'))
+app.engine('hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extname:'.hbs'
+}));
+app.set('view engine','.hbs');
 
 /* ------------------------------- Middlewares ------------------------------ */
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 
 /* --------------------------------- Routes --------------------------------- */
+app.get('/', (req, res) => {
+    res.json({"message": "Hola Mundo"})
+});
 
 /* ------------------------------ Public files ------------------------------ */
+app.use(express.static(join(__dirname, 'public')))
 
 /* ------------------------------- Run server ------------------------------- */
 app.listen(app.get('port'), () => {
