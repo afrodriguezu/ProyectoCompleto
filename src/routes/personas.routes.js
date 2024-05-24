1e4b562b835d3450f156ff0e55d6020122b2e2fe
afrodriguezu <andrey498@hotmail.com>
2024-05-23 21:49:28 -0500

    Creamos la vista add y la ruta y el post del add

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -3,11 +3,37 @@ import pool from '../database.js'
 
 const router = Router();
 
+router.get('/add', (req, res) => {
+    res.render('personas/add')
+});
+
+router.post('/add', async(req, res) => {
+    try {
+        const { name, lastname, age} = req.body
+        const newPersona = {
+            name, lastname, age
+        }
+        await pool.query('INSERT INTO personas SET ?', [newPersona]);
+        res.redirect('/list');
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
 router.get('/list', async(req, res) => {
     try {
         const [result] = await pool.query('SELECT * FROM personas');
         res.render('personas/list', {personas: result})
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+router.get('/delete/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        await pool.query('DELETE FROM personas WHERE id = ?', [id]);
+        res.redirect('/list');
     } catch (error) {
         res.status(500).json({message: error.message});
     }
