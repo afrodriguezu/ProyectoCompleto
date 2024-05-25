882d5491f0bb7efbe44627de30e0474b33f36bfc
afrodriguezu <andrey498@hotmail.com>
2024-05-24 22:52:00 -0500

    Se creo campo Observacion

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -9,9 +9,9 @@ router.get('/add', (req, res) => {
 
 router.post('/add', async(req, res) => {
     try {
-        const { name, lastname, age} = req.body
+        const { name, lastname, age, observacion} = req.body
         const newPersona = {
-            name, lastname, age
+            name, lastname, age, observacion
         }
         await pool.query('INSERT INTO personas SET ?', [newPersona]);
         res.redirect('/list');
@@ -53,11 +53,12 @@ router.get('/edit/:id', async(req, res) => {
 router.post('/edit/:id', async(req, res) => {
     try {
         const {id} = req.params
-        const {name, lastname, age} = req.body
+        const {name, lastname, age, observacion} = req.body
         const editPersona = {
                                 name,
                                 lastname,
-                                age
+                                age,
+                                observacion
                             }
         await pool.query('UPDATE personas SET ? WHERE id = ?', [editPersona, id]);
         res.redirect('/list');
